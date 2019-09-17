d5fd3e7c0b7a775436ea25cd6ed5deea076c1baf
Jackie Chen <ecuojackie@yahoo.com>
2019-09-16 19:36:47 -0700

    Displaying names and titles from league API

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
     return (
         <nav className="nav-wrapper blue darken-4">
             <div className="container">
-                <div className="brand-logo">InfoWatch</div>
+                <div className="brand-logo">LeagueInfo</div>
                 <ul className="right">
                     <li><NavLink to="/">Home</NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
--- src/components/home.js
+++ src/components/home.js
@@ -1,49 +1,48 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 import Input from './Input'
 
-class Home extends Component{
-    state = {
-        champions: []
-    }
-    componentWillMount(){
-        axios.get('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
+export default function Home () {
+    const [champions, setChampions] = useState([]);
+    useEffect(() => {
+        let cancel
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        axios.get(`${proxy}http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json `, {
+            cancelToken: new axios.CancelToken( c => {
+                cancel = c
+            })
+        })
             .then(res => {
-                this.setState({
-                    champions: res.data.data
-                })
+                setChampions(res.data.data)
             })
-    }
-    render(){
-        const {champions} = this.state;
-        const championPool = champions.length ? (
-            champions.map(champ => {
-                return (
-                    <div className="post card" key={champ.key}>
-                        <div className="card-content">
-                            <Link to={'/' + champ.id} >
-                            <span className="card-title red-text">{champ.id}</span>
-                            </Link>
-                            <p>{champ.title}</p>
-                        </div>  
-                    </div>
-                )
-            })
-        ) : (
-            <div className="center">No results were found</div>
-        )
-        console.log(champions)
+        return () => cancel();
+    }, []);
+
+    const champArray = Object.values(champions)
+    const champPool = champArray ? champArray.map((champ) => {
+        console.log(champ)
         return(
-            <div className="container home">
-                <h6 className="center">Type below to search</h6>
-                <Input />
-                <div className="results">
-                    {championPool}
-                </div>
+            <div className="post card" key={champ.key}>
+                <Link to={'/' + champ.id} >
+                    <img src={champ.image} alt="splash-art"/>
+                    <div className="card-content">
+                        <span className="card-title red-text">{champ.id}</span>
+                        <p>{champ.title}</p>
+                    </div>  
+                </Link>
             </div>
         );
-    }
+    }) : (
+        <div className="center no-results">No results were found</div>
+        )
+    return (
+        <div className="container home">
+            <h6 className="center">Type below to search</h6>
+            <Input />
+            <div className="results">
+                {champPool}
+            </div>
+        </div>
+    );
 }
-
-export default Home;
