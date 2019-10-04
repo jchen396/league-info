499af45aad3bde7cf79aae7f1ae277c6a2e73323
Jackie Chen <ecuojackie@yahoo.com>
2019-10-04 16:39:00 -0700

    Updates

---
+++ precache-manifest.1335b380af358b04115df611515e5646.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4cb1e67207c2ad0ddf2ba46b678ecf",
+    "url": "/league-info/index.html"
+  },
+  {
+    "revision": "a5db35c5761385e8244b",
+    "url": "/league-info/static/css/main.ae95a4c4.chunk.css"
+  },
+  {
+    "revision": "f3b099843cab23590cbe",
+    "url": "/league-info/static/js/2.33506e3c.chunk.js"
+  },
+  {
+    "revision": "a5db35c5761385e8244b",
+    "url": "/league-info/static/js/main.b6a0a161.chunk.js"
+  },
+  {
+    "revision": "f1f57072f9f451248999",
+    "url": "/league-info/static/js/runtime~main.21a7e22f.js"
+  },
+  {
+    "revision": "39964f7f7440521c5d53a63cbeed96ff",
+    "url": "/league-info/static/media/Emblem_Bronze.39964f7f.png"
+  },
+  {
+    "revision": "4aaf9754594ca11cbbe6a146ffb0eaa1",
+    "url": "/league-info/static/media/Emblem_Challenger.4aaf9754.png"
+  },
+  {
+    "revision": "c901832766fb4f856d1f9603208c6761",
+    "url": "/league-info/static/media/Emblem_Diamond.c9018327.png"
+  },
+  {
+    "revision": "19be519b965d28bf5af7cbb6b7f2471a",
+    "url": "/league-info/static/media/Emblem_Gold.19be519b.png"
+  },
+  {
+    "revision": "d0d4b62f7415cf0ca06d60ce1ae36744",
+    "url": "/league-info/static/media/Emblem_Grandmaster.d0d4b62f.png"
+  },
+  {
+    "revision": "1a6b4669f3cb62c2edf32971da0b146e",
+    "url": "/league-info/static/media/Emblem_Iron.1a6b4669.png"
+  },
+  {
+    "revision": "4156f1c160999db19a69d5bb0e2aaca7",
+    "url": "/league-info/static/media/Emblem_Master.4156f1c1.png"
+  },
+  {
+    "revision": "86ac5170561afe99a457dccd664eaf72",
+    "url": "/league-info/static/media/Emblem_Platinum.86ac5170.png"
+  },
+  {
+    "revision": "3a4aa1a5e576ae0ffb79db1f7505ba37",
+    "url": "/league-info/static/media/Emblem_Silver.3a4aa1a5.png"
+  },
+  {
+    "revision": "12ca6a0760cd8c0348aea909536e216e",
+    "url": "/league-info/static/media/Emblem_null.12ca6a07.png"
+  }
+]);
