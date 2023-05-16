25ce24ba6eff19c26a2e95b6c9984611680887f8
Jackie Chen <ecuojackie@yahoo.com>
2023-05-15 23:14:57 -0700

    Fix links in navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,37 +2,37 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-    const [active, setActive] = useState(false);
-    const [classList, setClassList] = useState("responsive");
-    useEffect(() => {
-        if (active) setClassList("right responsive");
-        else setClassList("right");
-    }, [active]);
-    return (
-        <nav className="nav-wrapper">
-            <div className="container">
-                <div className="brand-logo">
-                    <NavLink to="/">LeagueInfo</NavLink>
-                </div>
-                <ul className={classList}>
-                    <li onClick={() => setActive(false)}>
-                        <NavLink to="/">Champions</NavLink>
-                    </li>
-                    <li onClick={() => setActive(false)}>
-                        <NavLink to="/players">Players</NavLink>
-                    </li>
-                </ul>
-                <div
-                    className="hamburger"
-                    onClick={() =>
-                        active ? setActive(false) : setActive(true)
-                    }
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </div>
-        </nav>
-    );
+	const [active, setActive] = useState(false);
+	const [classList, setClassList] = useState("responsive");
+	useEffect(() => {
+		if (active) setClassList("right responsive");
+		else setClassList("right");
+	}, [active]);
+	return (
+		<nav className="nav-wrapper">
+			<div className="container">
+				<div className="brand-logo">
+					<NavLink to="/">LeagueInfo</NavLink>
+				</div>
+				<ul className={classList}>
+					<li onClick={() => setActive(false)}>
+						<NavLink to="/champions">Champions</NavLink>
+					</li>
+					<li onClick={() => setActive(false)}>
+						<NavLink to="/players">Players</NavLink>
+					</li>
+				</ul>
+				<div
+					className="hamburger"
+					onClick={() =>
+						active ? setActive(false) : setActive(true)
+					}
+				>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+		</nav>
+	);
 }
